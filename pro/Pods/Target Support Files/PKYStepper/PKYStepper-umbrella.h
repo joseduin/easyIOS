43f71a6c7f2feb1de80009e8391a504527ef1f1a
Kevin Garcia <kevingarcia@learniosfreecoms-mac.local>
2016-12-28 04:20:58 -0400

    c

--- pro/Pods/Target Support Files/PKYStepper/PKYStepper-umbrella.h
+++
@@ -1,9 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-#import "PKYStepper.h"
-
-FOUNDATION_EXPORT double PKYStepperVersionNumber;
-FOUNDATION_EXPORT const unsigned char PKYStepperVersionString[];
-
--- pro/Pods/Target Support Files/Pods-market/Pods-market-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_marketVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_marketVersionString[];
-
